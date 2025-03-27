d7c37cc7bfe40fc0f651064bb6b60637878acf86
Marcos Martins <164579513+marcosmartins2@users.noreply.github.com>
2025-03-27 18:40:47 -0300

    Adiciona estilo ao h1, atualiza app.component.html para incluir app-home e modifica home.component.html e home.component.ts para exibir informações do usuário.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
 
+
+export interface Pessoa{
+  nome:string
+  idade:number
+  cidade:string
+  profissao:string
+}
+
 @Component({
   selector: 'app-home',
   imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
 
+
+export class HomeComponent {
+    usuario: Pessoa = {
+        nome: 'Lucas',
+        idade:22,
+        cidade: 'São Paulo',
+        profissao: 'Desenvolvedor',
+    }
 }
+
